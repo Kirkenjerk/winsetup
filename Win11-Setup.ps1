6ca65370f019f1f8b7e71b82d7824e85d341e617
Kirkenjerk <kirkenjerk@gmail.com>
2021-10-05 11:45:53 -0700

    Initial Commit

---
+++ Win11-Setup.ps1
@@ -0,0 +1,15 @@
+#Runs powershell window as admin if not already elevated.
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+# Sets execution policy to bypass in order to run scripts.
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Assigns txt file with list of apps to a variable, then runs a command against each app in the txt file.
+$applist = Get-Content -Path C:\\Win11-Setup\apps.txt
+
+foreach ($app in $applist) {
+    choco install $app -y -f
+}
